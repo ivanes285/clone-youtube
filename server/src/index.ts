f414fc53d628f0a77c42e8bc36c993a3c4ce34e8
ivanes285 <ivanes285@gmail.com>
2023-01-09 16:41:20 -0500

    Fix value for port

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,6 @@ import {app} from './server';
 import colors from 'colors';
 
 
-app.listen(4000, () => {
+app.listen(process.env.PORT!, () => {
     console.log(  `Server is running on port ${process.env.PORT}  ${colors.blue('http://localhost:4000/api/v1/') } `);
     });
