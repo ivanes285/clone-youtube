db56928c88fadbdb37035827e79439d588dde2c2
ivanes285 <ivanes285@gmail.com>
2023-01-09 19:58:37 -0500

    Add funtions for the routes

---
+++ server/src/controllers/v1/user.controller.ts
@@ -0,0 +1,201 @@
+import { Request, Response } from 'express';
+import User from '../../models/v1/User';
+import IError from '../../interfaces/IError';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { Types } from 'mongoose';
+
+const signup = async (req: Request, res: Response) => {
+    try {
+        const { name, email } = req.body;
+        const existUser = await User.findOne({ name });
+        const existEmail = await User.findOne({ email });
+        if (existUser) {
+            throw {
+                code: 400,
+                message: 'Name already exist please use another '
+            };
+        }
+        if (existEmail) {
+            throw {
+                code: 400,
+                message: 'Email already exist please use another'
+            };
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+        const newUser = new User({ ...req.body, password: hashPassword });
+        await newUser.save();
+        res.status(200).json({ message: 'User created' });
+    } catch (error) {
+        const typedError = error as IError;
+        res.status(typedError.code).json({ message: typedError.message });
+    }
+};
+
+const signin = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw { code: 400, message: 'User not found' };
+        }
+        const validPassword: boolean = await bcrypt.compare(
+            password,
+            user.password
+        );
+        if (!validPassword) {
+            throw { code: 400, message: 'Invalid password' };
+        }
+        const expiresIn = 60 * 60;
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, {
+            expiresIn
+        });
+        const userFind = await User.findOne({ email }).select('-password');
+        res.cookie('token', token, { httpOnly: true });
+        res.status(200).json(userFind);
+    } catch (error) {
+        const typedError = error as IError;
+        res.status(typedError.code).json({ message: typedError.message });
+    }
+};
+
+const getUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        const typedError = error as IError;
+        res.status(typedError.code).json({ message: typedError.message });
+    }
+};
+
+const getUserById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        if (!Types.ObjectId.isValid(id)) {
+            throw { code: 400, message: 'ID inválido' };
+          }
+        const userFound = await User.findById(id);
+        if (!userFound) {
+            throw { code: 404, message: 'User not found' };
+        }
+         res.status(200).json(userFound);
+
+    } catch (error) {
+        const typedError = error as IError;
+        res.status(typedError.code).json({ message: typedError.message });
+    }
+};
+
+const updateUser = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        
+        if (!Types.ObjectId.isValid(id)) {
+            throw { code: 400, message: 'ID inválido' };
+          }
+
+        if ((id as string) !== req.user.id) {
+            throw { code: 403, message: 'You can update only your account' };
+        }
+
+        const updateUser = await User.findByIdAndUpdate(
+            id,
+            { ...req.body },
+            { new: true }
+        );
+        if (updateUser) {
+            // res.status(200).json({ message: 'User updated' });
+            res.status(200).json(updateUser);
+        }
+    } catch (error) {
+        const typedError = error as IError;
+        res.status(typedError.code).json({ message: typedError.message });
+    }
+};
+
+
+const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        if (!Types.ObjectId.isValid(id)) {
+            throw { code: 400, message: 'ID inválido' };
+          }
+          if ((id as string) !== req.user.id) {
+            throw { code: 403, message: 'You can delete only your account' };
+        }
+
+        const userDelete = await User.findByIdAndDelete(id);
+
+        if (!userDelete) {
+            throw { code: 404, message: 'User not found' };
+        }
+        res.status(200).json({ message: 'User account deleted' });
+    } catch (error) {
+        const typedError = error as IError;
+        res.status(typedError.code).json({ message: typedError.message });
+    }
+};
+
+const subscribeUser = async (req: Request, res: Response) => {
+    try {
+        const myId=req.user.id;
+        const { id } = req.params;
+        await User.findByIdAndUpdate(myId,{$push:{suscribedUsers:id}});
+         await User.findByIdAndUpdate(id,{$inc:{suscribers:1}})
+
+        res.status(200).json({ message: 'Subscription succesfull' });
+    } catch (error: any) {
+        //
+        res.status(error.code).json({ message: error.message });
+    }
+};
+
+const unSubscribeUser = async (req: Request, res: Response) => {
+    try {
+        const myId=req.user.id;
+        const { id } = req.params;
+        await User.findByIdAndUpdate(myId,{$pull:{suscribedUsers:id}});
+         await User.findByIdAndUpdate(id,{$inc:{suscribers:-1}})
+
+        res.status(200).json({ message: 'Subscription succesfull' });
+    } catch (error: any) {
+        //
+        res.status(error.code).json({ message: error.message });
+    }
+};
+
+const likeVideo = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+    } catch (error) {
+        //
+    }
+};
+
+const disLikeVideo = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+    } catch (error) {
+        //
+    }
+};
+
+
+
+
+export {
+    signup,
+    signin,
+    getUsers,
+    updateUser,
+    deleteUser,
+    getUserById,
+    subscribeUser,
+    unSubscribeUser,
+    likeVideo,
+    disLikeVideo
+};
