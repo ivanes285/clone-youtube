b20602fb5fd01aed2483e8c249b80155418dba27
ivanes285 <ivanes285@gmail.com>
2023-01-09 20:00:03 -0500

    Fix funtions in routes

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,12 +1,55 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 const router = Router();
+import {
+    deleteUser,
+    disLikeVideo,
+    getUserById,
+    getUsers,
+    likeVideo,
+    signin,
+    signup,
+    subscribeUser,
+    unSubscribeUser,
+    updateUser
+} from '../controllers/v1/user.controller';
+import { Auth } from '../middlewares/auth.middleware';
 
-router.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'Everything is ok' });
-});
+//create a new user
+router.post('/signup', signup);
+
+//sign in
+router.post('/signin', signin);
+
+//google auth
+router.post('/google');
+
+//get Users
+router.get('/', getUsers);
+
+//update user
+router.put('/:id', Auth, updateUser);
+
+//delete user
+router.delete('/:id', Auth, deleteUser);
 
 
+//get user by id
+router.get('/:id', getUserById);
 
 
+//subscribe user
+router.put('/sub/:id', Auth, subscribeUser);
+
+
+//unsubscribe user
+router.put('/unsub/:id', Auth, unSubscribeUser);
+
+
+//like a video
+router.put('/like/:videoId', Auth, likeVideo);
+
+
+//dislike a video
+router.put('/dislike/:videoId', Auth, disLikeVideo);
 
 export default router;
