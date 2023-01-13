0f866bfa3f740fd97c945e8714c1a4332f55fe57
ivanes285 <ivanes285@gmail.com>
2023-01-13 09:38:16 -0500

    Fix routes

--- server/src/routes/video.routes.ts
+++ server/src/routes/video.routes.ts
@@ -1,43 +1,58 @@
-import {Router } from 'express';
-import { createVideo,updateVideo,deleteVideo, getVideoById, getVideos,addView,random, trend,sub,allVideos } from '../controllers/v1/video.controller';
+import { Router } from 'express';
+import {
+    createVideo,
+    updateVideo,
+    deleteVideo,
+    getVideoById,
+    getVideos,
+    addView,
+    random,
+    trend,
+    sub,
+    allVideos,
+    getByTag,
+    search
+} from '../controllers/v1/video.controller';
 const router = Router();
 import { Auth } from '../middlewares/auth.middleware';
 
-//get all videos by user
-router.get('/sub',Auth, sub) 
-
-//Get all videos
-router.get('/all',allVideos)
 
 //Get all videos for id
-router.get('/',Auth,getVideos)
+router.get('/', Auth, getVideos);
+
+//Get all videos
+router.get('/all', allVideos);
+
+//get random videos
+ router.get('/random', random);
+
+//get all videos by views
+ router.get('/trend', trend);
+
+
+//get a video
+ router.get('/find/:id', Auth, getVideoById);
+ 
+ router.get('/tags', getByTag);
 
 //Create a new video
-router.post('/',Auth,createVideo)
+router.post('/', Auth, createVideo);
+
+//get all videos by user
+router.get('/sub', Auth, sub);
+
+
+router.get('/search', search);
 
 // update a video
-router.put('/:id',Auth,updateVideo)
-
-//delete a video
-router.delete('/:id',Auth,deleteVideo)
-
-//get a video 
-router.get('/:id',Auth,getVideoById)
+router.put('/:id', Auth, updateVideo);
 
 
 //add view
-router.put('/view/:id',addView)
+router.put('/view/:id', addView);
 
-
-//get all videos by views
-router.get('/trend',trend)
-
-//get random videos
-router.get('/random',random)
-
-
-
-
+//delete a video
+router.delete('/:id', Auth, deleteVideo);
 
 
 export default router;
