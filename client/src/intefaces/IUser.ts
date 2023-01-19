5ad719d321b5b8389482bee4aca65145b79368ec
ivanes285 <ivanes285@gmail.com>
2023-01-19 12:05:00 -0500

    Add Interfaces

---
+++ client/src/intefaces/IUser.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+    _id: string;
+    name: string;
+    email: string;
+    password: string;
+    img:string;
+    suscribers: number;
+    suscribedUsers: string[];
+  
+
+}
