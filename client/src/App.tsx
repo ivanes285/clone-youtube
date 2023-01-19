be1ec987bf52541d08e5de4a9d54b288d4579fe1
ivanes285 <ivanes285@gmail.com>
2023-01-19 12:00:23 -0500

    Add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import Video from "./pages/Video";
 
+
 const Container = styled.div`
   display: flex;
 `;
@@ -18,7 +19,6 @@ const Main = styled.div`
 const Wrapper = styled.div`
   padding: 22px 96px;
 `;
-
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -32,7 +32,9 @@ function App() {
             <Wrapper>
               <Routes>
                 <Route path="/">
-                  <Route index element={<Home />} />
+                  <Route index element={<Home type="random"/>} />
+                  <Route path="trends" element={<Home type="trend" />} />
+                  <Route path="subcriptions" element={<Home type="sub" />} />
                   <Route path="signin" element={<SignIn />} />
                   <Route path="video">
                     <Route path=":id" element={<Video />} />
