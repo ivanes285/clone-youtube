edd43715ab0d4312329c695f38726cf0b046bb5f
ivanes285 <ivanes285@gmail.com>
2023-01-20 00:05:51 -0500

    Add slice for video  and methods for manage state

---
+++ client/src/redux/videoSlice.ts
@@ -0,0 +1,60 @@
+import { IUser } from "@/intefaces/IUser";
+import { IVideo } from "@/intefaces/IVideo";
+import { createSlice } from "@reduxjs/toolkit";
+
+interface VideoState {
+  currentVideo: IVideo | null;
+  loading: boolean;
+  error: boolean;
+}
+
+const initialState: VideoState = {
+  currentVideo: null,
+  loading: false,
+  error: false,
+};
+
+export const videoSlice = createSlice({
+  name: "video",
+  initialState,
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+    },
+
+    fetchSuccess: (state, action) => {
+      state.loading = false;
+      state.currentVideo = action.payload;
+    },
+
+    fetchFailure: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+
+    like: (state, action) => {
+      if (!state.currentVideo?.likes.includes(action.payload)) {
+        state.currentVideo?.likes.push(action.payload);
+        state.currentVideo?.dislikes.splice(
+          state.currentVideo?.dislikes.findIndex((userId) => userId === action.payload),
+          1
+        );
+      }
+    },
+
+    dislike: (state, action) => {
+      if (!state.currentVideo?.dislikes.includes(action.payload)) {
+        state.currentVideo?.dislikes.push(action.payload);
+        state.currentVideo?.likes.splice(
+          state.currentVideo?.likes.findIndex((userId) => userId === action.payload),
+          1
+        );
+      }
+    },
+    
+  },
+});
+
+export const { fetchStart, fetchSuccess, fetchFailure, like, dislike } = videoSlice.actions;
+
+export default videoSlice.reducer;
